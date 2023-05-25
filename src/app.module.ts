2641be390e5a13716cbcc757aad55ac54c1fcf1e
vietanhcao <vietanhcao1994@gmail.com>
2023-05-25 11:03:10 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { NoteModule } from './note/note.module';
+
+@Module({
+  imports: [AuthModule, UserModule, NoteModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
