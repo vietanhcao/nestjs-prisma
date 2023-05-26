91d58e985bc3a2095163a5b27e61e14b32f3f39b
vietanhcao <vietanhcao1994@gmail.com>
2023-05-26 15:50:29 +0700

    feat(init): add custom decorate

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { MyJwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(MyJwtGuard)
   @Get('me')
-  me(@Req() req: Request) {
-    return req.user;
+  me(@GetUser() user: User) {
+    return user;
   }
 }
